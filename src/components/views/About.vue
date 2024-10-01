9527ab1d1d6dd2d86f5e447e8d1566092d99be4f
mineot <mineot@gmail.com>
2024-09-30 22:37:45 -0300

    master - remove old project

--- src/components/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="page">
-    <div class="title">About</div>
-    <div class="content">About Content</div>
-  </div>
-</template>
--- src/preload.ts
+++
@@ -1,5 +0,0 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-
-import { Storage } from './core/storage';
-window.storage = new Storage();
